f0cf23b0043764b60014f105c61be9bd08b4b284
Matheus G. Mussi <matheus.mussi1996@hotmail.com>
2022-05-30 21:10:38 -0300

    Read .xlxs file

--- dataframe_stats.py
+++ dataframe_stats.py
@@ -5,3 +5,11 @@ Created on Mon May 30 20:41:45 2022
 
 @author: Matheus_Mussi
 """
+import pandas as pd
+
+df = pd.read_excel('__Dataframe_ScopingReview.xlsx', sheet_name=1, header=[0])
+pd.set_option('display.max_rows',20)
+pd.set_option('display.max_columns', None)
+print(df)
+print(df['ID'].values)
+print(df['On_ACC'].values)
